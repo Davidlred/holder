01f0d1723673e8d9e143896b1e5c77a0b31cca78
davidlred <davidprincewill20@gmail.com>
2023-06-29 08:27:39 -0700

    do hard things

--- convert_oct.c
+++ convert_oct.c
@@ -1,25 +1,26 @@
 #include "main.h"
 /**
  * convert_oct - Prints an unsigned integer in octal base
- * @val: Integer tobe converted
+ * @val: number to be converted
  * @c: Pointer to character
  * Return: Void
  */
 void convert_oct(unsigned long int val, int *c)
 {
-	unsigned long int num;
-	
-	unsigned long int rtn;
+	unsigned long int a;
+
+	unsigned long int b;
 
 	if (val <= 7)
 	{
-		*c += _putchar(val + '0');
+		*c = *c +  _putchar(val + '0');
 		return;
 	}
 
-	num = val / 8;
-	rtn = val % 8;
+	a = val / 8;
 
-	convert_oct(num, c);
-	*c += _putchar(rtn + '0');
+	b = val % 8;
+
+	convert_oct(a, c);
+	*c = *c + _putchar(b + '0');
 }
